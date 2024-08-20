6354740320b03a10b7cd35c41b0d24418722afe2
Yessenia Becerra <phoebe93xenia@gmail.com>
2024-08-20 00:15:37 -0600

    Adding Workflow

--- initial-script.sh
+++ initial-script.sh
@@ -19,24 +19,16 @@ sudo snap install kubectl --classic
 sudo apt install -y docker.io
 
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-
 TAGS=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s "http://169.254.169.254/latest/meta-data/tags/instance")
+NODE_TYPE=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s "http://169.254.169.254/latest/meta-data/tags/instance/Node-Type")
 
-TAG_VALUE=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s "http://169.254.169.254/latest/meta-data/tags/instance/Name")
-
-echo "Instance tags: $TAGS"
-echo "Value of the 'Name' tag: $TAG_VALUE"
-
-if [ "$TAG_VALUE" == "Master" ]; then
+if [ "$NODE_TYPE" == "Master" ]; then
     curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --cluster-init
     aws ssm put-parameter --name "/k3s/cluster/token" --value "$(cat /var/lib/rancher/k3s/server/node-token)" --type SecureString
+    aws ssm put-parameter --name "/k3s/cluster/master-ip" --value "$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s "http://169.254.169.254/latest/meta-data/local-ipv4")" --type String --overwrite
 else
-    TOKEN=$(aws ssm get-parameter --name "/k3s/cluster/token" --with-decryption --query "Parameter.Value" --output text)
-    SERVER_URL="https://<IP_O_DNS_DEL_SERVER>:6443"
-    curl -sfL https://get.k3s.io | K3S_URL=$SERVER_URL K3S_TOKEN=$TOKEN sh -s - server --cluster-init --disable-apiserver --disable-controller-manager --disable-scheduler
+    KS_TOKEN=$(aws ssm get-parameter --name "/k3s/cluster/token" --with-decryption --query "Parameter.Value" --output text)
+    MASTER_IP=$(aws ssm get-parameter --name "/k3s/cluster/master-ip" --query "Parameter.Value" --output text)
+    SERVER_URL="https://$MASTER_IP:6443"
+    curl -sfL https://get.k3s.io | K3S_URL=$SERVER_URL K3S_TOKEN=$KS_TOKEN sh -s - server --cluster-init --disable-apiserver --disable-controller-manager --disable-scheduler
 fi
-
-
-
-
-
---
+++ win_script.ps1
@@ -0,0 +1,38 @@
+<powershell>
+# Crear un usuario por defecto
+$username = "trendmicro"
+$password = ConvertTo-SecureString "trendmicro" -AsPlainText -Force
+New-LocalUser -Name $username -Password $password -FullName "trendmicro" -Description "User for trendmicro access"
+Add-LocalGroupMember -Group "Administrators" -Member $username
+
+# Instalar Google Chrome
+$chromeInstaller = "$env:USERPROFILE\Downloads\chrome_installer.exe"
+Invoke-WebRequest "https://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $chromeInstaller
+Start-Process -FilePath $chromeInstaller -ArgumentList "/silent /install" -Wait
+
+# Instalar Visual Studio Code
+$vscodeInstaller = "$env:USERPROFILE\Downloads\vscode_installer.exe"
+Invoke-WebRequest "https://update.code.visualstudio.com/latest/win32-x64-user/stable" -OutFile $vscodeInstaller
+Start-Process -FilePath $vscodeInstaller -ArgumentList "/silent" -Wait
+
+# Instalar Git
+$gitInstaller = "$env:USERPROFILE\Downloads\git_installer.exe"
+Invoke-WebRequest "https://github.com/git-for-windows/git/releases/download/v2.34.1.windows.1/Git-2.34.1-64-bit.exe" -OutFile $gitInstaller
+Start-Process -FilePath $gitInstaller -ArgumentList "/VERYSILENT" -Wait
+
+# Configurar pestañas predeterminadas en Chrome para el Kubernetes Dashboard
+$chromePrefsPath = "$env:APPDATA\Google\Chrome\User Data\Default\Preferences"
+# Obtener MASTER_IP desde SSM Parameter Store
+$masterIp = (Get-SSMParameterValue -Name "/k3s/cluster/master-ip").Value
+# Configuración del entorno utilizando MASTER_IP
+$dashboardUrl = "http://$masterIp:6443/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
+$chromePrefs = @{
+    "session" = @{
+        "restore_on_startup" = 4
+        "urls_to_restore_on_startup" = @($dashboardUrl)
+    }
+}
+
+$chromePrefs | ConvertTo-Json | Set-Content -Path $chromePrefsPath
+
+</powershell>
