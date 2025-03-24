fa764afd13a9af00947f0b8887c8f7bb988930e0
Yessenia Becerra <phoebe93xenia@gmail.com>
2025-03-23 20:14:41 -0600

    Update initial-script.sh

--- initial-script.sh
+++ initial-script.sh
@@ -36,7 +36,7 @@ if [ "$NODE_TYPE" == "Master" ]; then
     KS_TOKEN=$(aws ssm get-parameter --name "/k3s/cluster/CS-token" --with-decryption --query "Parameter.Value" --output text)
     MASTER_IP=$(aws ssm get-parameter --name "/k3s/cluster/CS-master-ip" --query "Parameter.Value" --output text)
     SERVER_URL="https://$MASTER_IP:6443"
-    COMMAND="curl -sfL https://get.k3s.io | K3S_URL=$SERVER_URL K3S_TOKEN=$KS_TOKEN sh -"
+    #COMMAND="curl -sfL https://get.k3s.io | K3S_URL=$SERVER_URL K3S_TOKEN=$KS_TOKEN sh -"
     export KUBECONFIG="/etc/rancher/k3s/k3s.yaml"
 else
     KS_TOKEN=$(aws ssm get-parameter --name "/k3s/cluster/CS-token" --with-decryption --query "Parameter.Value" --output text)
