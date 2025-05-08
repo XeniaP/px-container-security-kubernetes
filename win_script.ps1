7ca10390909887d4e7724fbfd502dd62ecdd1809
Yessenia Becerra <phoebe93xenia@gmail.com>
2025-05-08 00:22:43 -0600

    Update win_script.ps1

--- win_script.ps1
+++ win_script.ps1
@@ -1,37 +1,23 @@
 # Define primero las variables
-net user adminuser "ContraseñaSegura123!" /add
+net user adminuser "TrendMicr01!" /add
 net localgroup administrators adminuser /add
 
-Get-NetFirewallRule -DisplayGroup "Remote Desktop" | Where-Object {$_.Enabled -eq "True"}
-Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+Set-ExecutionPolicy Bypass -Scope Process -Force
 
-# Instalar Google Chrome
-$chromeInstaller = "$env:USERPROFILE\Downloads\chrome_installer.exe"
-Invoke-WebRequest "https://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $chromeInstaller
-Start-Process -FilePath $chromeInstaller -ArgumentList "/silent /install" -Wait
+$temp = "$env:TEMP\installers"
+New-Item -ItemType Directory -Force -Path $temp | Out-Null
 
-# Instalar Visual Studio Code
-$vscodeInstaller = "$env:USERPROFILE\Downloads\vscode_installer.exe"
-Invoke-WebRequest "https://update.code.visualstudio.com/latest/win32-x64-user/stable" -OutFile $vscodeInstaller
-Start-Process -FilePath $vscodeInstaller -ArgumentList "/silent" -Wait
+$chromeUrl = "https://dl.google.com/chrome/install/375.126/chrome_installer.exe"
+$vscodeUrl = "https://update.code.visualstudio.com/latest/win32-x64-user/stable"
+$gitUrl = "https://github.com/git-for-windows/git/releases/download/v2.45.1.windows.1/Git-2.45.1-64-bit.exe"
+$awscliUrl = "https://awscli.amazonaws.com/AWSCLIV2.msi"
 
-# Instalar Git
-$gitInstaller = "$env:USERPROFILE\Downloads\git_installer.exe"
-Invoke-WebRequest "https://github.com/git-for-windows/git/releases/download/v2.34.1.windows.1/Git-2.34.1-64-bit.exe" -OutFile $gitInstaller
-Start-Process -FilePath $gitInstaller -ArgumentList "/VERYSILENT" -Wait
+Invoke-WebRequest $chromeUrl -OutFile "$temp\chrome_installer.exe"
+Invoke-WebRequest $vscodeUrl -OutFile "$temp\vscode_installer.exe"
+Invoke-WebRequest $gitUrl -OutFile "$temp\git_installer.exe"
+Invoke-WebRequest $awscliUrl -OutFile "$temp\AWSCLIV2.msi"
 
-# Configurar pestañas predeterminadas en Chrome para el Kubernetes Dashboard
-$chromePrefsPath = "$env:APPDATA\Google\Chrome\User Data\Default\Preferences"
-# Obtener MASTER_IP desde SSM Parameter Store
-$masterIp = (Get-SSMParameterValue -Name "/k3s/cluster/CS-master-ip").Value
-# Configuración del entorno utilizando MASTER_IP
-$dashboardUrl = "http://$masterIp:6443/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
-$chromePrefs = @{
-    "session" = @{
-        "restore_on_startup" = 4
-        "urls_to_restore_on_startup" = @($dashboardUrl)
-    }
-}
-
-$chromePrefs | ConvertTo-Json | Set-Content -Path $chromePrefsPath
+Start-Process "$temp\chrome_installer.exe" -ArgumentList "/silent /install" -Wait
+Start-Process "$temp\vscode_installer.exe" -ArgumentList "/VERYSILENT /NORESTART" -Wait
+Start-Process "$temp\git_installer.exe" -ArgumentList "/VERYSILENT /NORESTART" -Wait
+Start-Process "msiexec.exe" -ArgumentList "/i $temp\AWSCLIV2.msi /qn" -Wait
